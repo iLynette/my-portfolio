cf2575f14b763300c7d6c1fb0837ef55f4c5f001
iLynette <lynetteacholah@icloud.com>
2022-01-24 17:20:24 +0300

    Add javascript linter,fix the p tag

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 const menuAction = document.querySelectorAll(".navbar-mobile li");
+const closeMenu = document.querySelector("#cross-sign");
+const menuList = document.querySelector(".mobile-nav");
+const mobileMenu = document.querySelector(".navbar-mobile");
+const mobileNav = document.querySelector(".navbar-mobile");
 let listMenu = Array.from(menuAction)
 
 function open() {
-    const mobileMenu = document.querySelector(".navbar-mobile");
     mobileMenu.classList.remove("hidden");
 }
 
-const menuList = document.querySelector(".mobile-nav");
 menuList.addEventListener("click", open);
-
-const closeMenu = document.querySelector("#cross-sign");
 
 
 function close() {
-    const mobileNav = document.querySelector(".navbar-mobile");
     mobileNav.classList.add("hidden")
 }
 closeMenu.addEventListener("click", close)
