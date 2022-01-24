94409a0c8396e208c1d82176cf2077e0aa307c8b
iLynette <lynetteacholah@icloud.com>
2022-01-24 18:03:15 +0300

    Fix esLint

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-const menuAction = document.querySelectorAll(".navbar-mobile li");
-const closeMenu = document.querySelector("#cross-sign");
-const menuList = document.querySelector(".mobile-nav");
-const mobileMenu = document.querySelector(".navbar-mobile");
-const mobileNav = document.querySelector(".navbar-mobile");
-let listMenu = Array.from(menuAction)
+const menuAction = document.querySelectorAll('.navbar-mobile li');
+const closeMenu = document.querySelector('#cross-sign');
+const menuList = document.querySelector('.mobile-nav');
+const mobileMenu = document.querySelector('.navbar-mobile');
+const mobileNav = document.querySelector('.navbar-mobile');
+const listMenu = Array.from(menuAction);
 
 function open() {
-    mobileMenu.classList.remove("hidden");
+  mobileMenu.classList.remove('hidden');
 }
 
-menuList.addEventListener("click", open);
-
+menuList.addEventListener('click', open);
 
 function close() {
-    mobileNav.classList.add("hidden")
+  mobileNav.classList.add('hidden');
 }
-closeMenu.addEventListener("click", close)
+closeMenu.addEventListener('click', close);
 
-listMenu.forEach((elem) => elem.addEventListener("click", close));
+listMenu.forEach((elem) => elem.addEventListener('click', close));
