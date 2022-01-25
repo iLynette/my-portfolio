a82eb21a9aac701d370961d9a72caa922abdcca2
iLynette <lynetteacholah@icloud.com>
2022-01-25 21:32:53 +0300

    Add css and js files

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-const menuAction = document.querySelectorAll(".navbar-mobile li");
-let listMenu = Array.from(menuAction)
+const menuAction = document.querySelectorAll('.navbar-mobile li');
+const listMenu = Array.from(menuAction);
 
 function open() {
-    const mobileMenu = document.querySelector(".navbar-mobile");
-    mobileMenu.classList.remove("hidden");
+  const mobileMenu = document.querySelector('.navbar-mobile');
+  mobileMenu.classList.remove('hidden');
 }
 
-const menuList = document.querySelector(".mobile-nav");
-menuList.addEventListener("click", open);
+const menuList = document.querySelector('.mobile-nav');
+menuList.addEventListener('click', open);
 
-const closeMenu = document.querySelector("#cross-sign");
-
+const closeMenu = document.querySelector('#cross-sign');
 
 function close() {
-    const mobileNav = document.querySelector(".navbar-mobile");
-    mobileNav.classList.add("hidden")
+  const mobileNav = document.querySelector('.navbar-mobile');
+  mobileNav.classList.add('hidden');
 }
-closeMenu.addEventListener("click", close)
+closeMenu.addEventListener('click', close);
 
-listMenu.forEach((elem) => elem.addEventListener("click", close))
+listMenu.forEach((elem) => elem.addEventListener('click', close));
+
+// windowpopup
+
+
