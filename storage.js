cabc77086810b33f65eb216587d734064eec1ecb
iLynette <lynetteacholah@icloud.com>
2022-01-27 20:47:10 +0300

    Get form values

--- storage.js
+++ storage.js
@@ -3,3 +3,12 @@ let formData = {
   email: '',
   message: '',
 };
+
+const forms = document.querySelector('form');
+if (localStorage.getItem('formData') != null) {
+  formData = JSON.parse(localStorage.getItem('formData'));
+  forms.name.value = formData.name;
+  forms.email.value = formData.email;
+  forms.message.value = formData.message;
+}
+
