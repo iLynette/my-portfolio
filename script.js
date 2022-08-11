9722a4c5754e1e154c8f025af09b7e1183c22593
iLynette <lynetteacholah@icloud.com>
2022-08-11 18:22:19 +0300

    Change the buttons display

--- script.js
+++ script.js
@@ -23,9 +23,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    mybutton.style.display = 'block';
   } else {
-    mybutton.style.display = "none";
+    mybutton.style.display = 'none';
   }
 }
 
