1bfa3c1b4a02ef1a758c95b986542f8ff1bb701b
iLynette <lynetteacholah@icloud.com>
2022-08-11 18:21:24 +0300

    Add button fucntionality

--- script.js
+++ script.js
@@ -18,3 +18,18 @@ function close() {
 closeMenu.addEventListener('click', close);
 
 listMenu.forEach((elem) => elem.addEventListener('click', close));
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
